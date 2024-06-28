90d8e58e09fd6ada819154e8b323ee0bc6211f7e
Vinicius Bastos <106217765+viniciusbbastos@users.noreply.github.com>
2024-06-28 16:58:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function limparTexto(textarea) {
     if (textarea.value === "Digite seu texto") {
         textarea.value = "";
@@ -22,15 +21,9 @@ function criptografar() {
     let caixaTexto = document.getElementById("conteudo__mostra__resultado");
 
     alternativo.style.display = "none";
-
-    if (alternativo.style.display === "none") {
-        if (screen.width > 768) {
-            imagem.style.display = "none";
-        }
-        textoInfo.style.display = "none";
-        alternativo.style.display = "flex";
-    }
-
+	
+	trocaCena(textoLimpo, alternativo, imagem, textoInfo);
+	
     if (textoLimpo.value !== "Digite seu texto") {
         caixaTexto.value = textoLimpo.value.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
         textoLimpo.value = "Digite seu texto";
@@ -46,13 +39,7 @@ function descriptografar() {
 
     alternativo.style.display = "none";
 
-    if (alternativo.style.display === "none") {
-        if (screen.width > 768) {
-            imagem.style.display = "none";
-        }
-        textoInfo.style.display = "none";
-        alternativo.style.display = "flex";
-    }
+    trocaCena(textoLimpo, alternativo, imagem, textoInfo);
 
     if (textoLimpo.value !== "Digite seu texto") {
         caixaTexto.value = textoLimpo.value.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
@@ -64,4 +51,18 @@ function copiar() {
     let caixaTexto = document.getElementById("conteudo__mostra__resultado");
 
     navigator.clipboard.writeText(caixaTexto.value);
+}
+
+function trocaCena(tl, alt, imag, ti) {
+	if (tl.value !== "Digite seu texto" && alt.style.display === "none") {
+        imag.style.display = "none";
+        ti.style.display = "none";
+        alt.style.display = "flex";
+    } else {
+        if (window.innerWidth > 1024) {
+            imag.style.display = "block";
+        }
+        ti.style.display = "flex";
+        alt.style.display = "none";
+    }
 }
